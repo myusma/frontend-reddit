433dab5f473b7138d3c84f5d51438e386de82929
myusma <yusmamehmet@gmail.com>
2022-12-19 17:28:01 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/notfound/NotFound";
+import Subreddit from "./pages/subreddit/Subreddit";
+import React from "react";
+import Header from "./components/header/Header";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+
+        <>
+
+
+            <Routes>
+
+                <Route path= "/" element={<Home/>}/>
+                <Route path="/subreddit/:id" element={<Subreddit/>}/>
+                <Route path="*" element={<NotFound/>}/>
+
+            </Routes>
+
+
+        </>
+
+    );
 }
 
 export default App;
